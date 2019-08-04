29b56e76d4072602196a19bb8720942e63ba5957
Jean-Ralph Aviles <jeanralph.aviles@gmail.com>
2019-08-04 00:15:38 -0700

    comment_parser: Pin python-magic to latest version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='comment_parser',
-    version='1.1.1',
+    version='1.1.2',
     description='Parse comments from various source files.',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -21,7 +21,7 @@ setup(
     long_description=readme(),
     long_description_content_type='text/markdown',
     packages=['comment_parser', 'comment_parser.parsers'],
-    install_requires=['python-magic>=0.4'],
+    install_requires=['python-magic==0.4.15'],
     test_suite='nose.collector',
     tests_require=['nose'],
     zip_safe=False)
