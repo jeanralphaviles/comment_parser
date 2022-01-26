7b6c709a7400cb0ad6efaf7e2768c1a27a7578ad
Jean-Ralph Aviles <jeanralph.aviles@gmail.com>
2022-01-25 20:35:01 -0800

    comment_parser: Fix pylint errors.
    * W1514: Using open without explicitly specifying an encoding
    * C0209: Formatting a regular string which could be a f-string

--- comment_parser/parsers/common.py
+++ comment_parser/parsers/common.py
@@ -57,8 +57,7 @@ class Comment():
     return self._text
 
   def __repr__(self):
-    return 'Comment(%s, %d, %s)' % (self._text, self._line_number,
-                                    self._multiline)
+    return f'Comment({self._text}, {self._line_number}, {self._multiline})'
 
   def __eq__(self, other):
     if isinstance(other, self.__class__):
